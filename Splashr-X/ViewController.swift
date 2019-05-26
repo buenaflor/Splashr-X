3437968631c9f28fd1381c047eb85dd5cc7e4463
Giancarlo Buenaflor <giancarlo_buenaflor@yahoo.com>
2019-05-26 18:14:17 +0200

    Initial Commit

---
+++ Splashr-X/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Splashr-X
+//
+//  Created by Gino on 26.05.19.
+//  Copyright © 2019 Giancarlo Buenaflor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
